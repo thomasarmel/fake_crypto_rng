92874bdec68e94b1498d935f419af206937628e1
Thomas Prévost <tprevost@smtp.i3s.unice.fr>
2024-10-16 18:19:23 +0200

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use rand_core::{CryptoRng, Error, RngCore};
+
+pub struct FakeCryptoRng {}
+
+impl RngCore for FakeCryptoRng {
+    fn next_u32(&mut self) -> u32 {
+        0
+    }
+
+    fn next_u64(&mut self) -> u64 {
+        0
+    }
+
+    fn fill_bytes(&mut self, dest: &mut [u8]) {
+        dest.fill(0)
+    }
+
+    fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), Error> {
+        dest.fill(0);
+        Ok(())
+    }
+}
+
+impl CryptoRng for FakeCryptoRng {}
